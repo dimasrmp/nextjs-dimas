6bca0595a1f4ee76b37a86c746440fe3a75b506a
dimasrmp <dimas.putra@salt.co.id>
2022-09-20 18:21:29 +0700

    make nft minting 2

--- utils/mint.js
+++ utils/mint.js
@@ -3,7 +3,7 @@ import dimasnft from "../DimasNft.json"
 
 export const mint = async () => {
 
-    const contractAddress = "0x46EA2840AA3D65dbcF6979cC2e8fE078f572e2E8";
+    const contractAddress = "0x15db7F47ED6699fBCCB02ec70a61273F7dB04d6a";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const address = signer.getAddress();
